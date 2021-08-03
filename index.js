bba4ea428440dae2286c539bd760e51c95cd1c09
anonymous-ops <anonymous.virat@hotmail.com>
2021-08-03 16:15:05 +0100

    virat
    food

---
+++ index.js
@@ -0,0 +1,11 @@
+const close = document.getElementsByClassName('btn1');
+  close.addEventListener("click",off);
+function off(){
+    if(close.style.display==="none"){
+      close.style.display==="flex"
+    }
+    else{
+      close.style.display==="none"
+    }
+    
+}
